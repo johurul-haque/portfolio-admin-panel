903d6941a57983f1ed2040a7c1b38f6ad01df6ad
Johurul Haque <johurulhaquejony@gmail.com>
2024-07-09 00:37:47 +0600

    serving static client assets from hono

--- api/app.ts
+++
@@ -1,7 +0,0 @@
-import { Hono } from 'hono';
-
-export const app = new Hono();
-
-const apiRoutes = app.basePath('/api').get('/', (c) => {
-  return c.json({ message: 'Hello Hono!' });
-});
---
+++ server/app.ts
@@ -0,0 +1,14 @@
+import { serveStatic } from '@hono/node-server/serve-static';
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+
+export const app = new Hono();
+
+app.use('*', logger());
+
+const apiRoutes = app.basePath('/api').get('/', (c) => {
+  return c.json({ message: 'Hello Hono!' });
+});
+
+app.get('*', serveStatic({ root: './client/dist' }));
+app.get('*', serveStatic({ path: './client/dist/index.html' }));
