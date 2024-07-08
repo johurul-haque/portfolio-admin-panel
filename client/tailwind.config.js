f2e54efb9d57d7c235f4611b007481c5141e7386
Johurul Haque <johurulhaquejony@gmail.com>
2024-07-08 22:37:39 +0600

    init shadcn

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,18 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [tailwindcssAnimate],
 };
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,18 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    proxy: {
+      '/api': 'http://localhost:8080',
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
